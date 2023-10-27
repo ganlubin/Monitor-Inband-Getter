56dc04032a888d700b1b0788ebbffde9f74bfb5c
ganlubin <ganlubin@mail.ustc.edu.cn>
2023-10-27 14:55:14 +0800

    completed system_info json file created.

--- src/jsonPackage.cpp
+++ src/jsonPackage.cpp
@@ -15,6 +15,11 @@ std::pair<std::string, int> findFromFile_system_info(const std::string &,
 int system_info(std::ifstream &, json &);
 int running_minutes(const std::pair<std::string, int> &);
 int system_users(const std::pair<std::string, int> &);
+void setSystem_load_average(json &, const std::pair<std::string, int> &);
+void system_tasks(json &, const std::pair<std::string, int> &);
+void system_cpus(json &, const std::pair<std::string, int> &);
+void system_memory(json &, const std::pair<std::string, int> &);
+void system_swap(json &, const std::pair<std::string, int> &);
 
 int create_info_json() {
   // clear cached file
@@ -94,9 +99,312 @@ int system_info(std::ifstream &Boost_process_info_system, json &json_info) {
   // system load average
   key = "load average";
   temp = findFromFile_system_info(Boost_process_info_system_content, key);
-  std::cout << temp.first[temp.second - 1] << std::endl;
+  if (temp.second == -1) {
+    printError("Could not find key string: load average");
+    return 0;
+  }
+  setSystem_load_average(json_info, temp); // 1 5 15 minutes
+
+  // system tasks
+  key = "Tasks";
+  temp = findFromFile_system_info(Boost_process_info_system_content, key);
+  if (temp.second == -1) {
+    printError("Could not find key string: Tasks");
+    return 0;
+  }
+  system_tasks(json_info, temp);
+
+  // system CPUs
+  key = "Cpu(s):";
+  temp = findFromFile_system_info(Boost_process_info_system_content, key);
+  if (temp.second == -1) {
+    printError("Could not find key string: Cpu(s)");
+    return 0;
+  }
+  system_cpus(json_info, temp);
+
+  // system Mem
+  key = "MiB Mem :";
+  temp = findFromFile_system_info(Boost_process_info_system_content, key);
+  if (temp.second == -1) {
+    printError("Could not find key string: MiB Mem");
+    return 0;
+  }
+  system_memory(json_info, temp);
+
+  // system Swap
+  key = "MiB Swap:";
+  temp = findFromFile_system_info(Boost_process_info_system_content, key);
+  if (temp.second == -1) {
+    printError("Could not find key string: MiB Swap");
+    return 0;
+  }
+  system_swap(json_info, temp);
 
   return 1;
+}
+
+void system_swap(json &json_info, const std::pair<std::string, int> &temp) {
+  std::string source = temp.first;
+  int idx = temp.second;
+  std::string res = "";
+
+  // total
+  for (; source[idx] != ','; idx++) {
+    if (source[idx] >= '0' && source[idx] <= '9' || source[idx] == '.') {
+      res += source[idx];
+    }
+  }
+  json_info["system"]["Swap(MiB)"]["total"] = atof(res.c_str());
+  res = "", idx++;
+
+  // free
+  for (; source[idx] != ','; idx++) {
+    if (source[idx] >= '0' && source[idx] <= '9' || source[idx] == '.') {
+      res += source[idx];
+    }
+  }
+  json_info["system"]["Swap(MiB)"]["free"] = atof(res.c_str());
+  res = "", idx++;
+
+  // used
+  for (; source[idx] != 'u'; idx++) {
+    if (source[idx] >= '0' && source[idx] <= '9' || source[idx] == '.') {
+      res += source[idx];
+    }
+  }
+  json_info["system"]["Swap(MiB)"]["used"] = atof(res.c_str());
+  res = "", idx += 5;
+
+  // avail
+  for (; source[idx] != 'a'; idx++) {
+    if (source[idx] >= '0' && source[idx] <= '9' || source[idx] == '.') {
+      res += source[idx];
+    }
+  }
+  json_info["system"]["Swap(MiB)"]["avail"] = atof(res.c_str());
+  return;
+}
+
+void system_memory(json &json_info, const std::pair<std::string, int> &temp) {
+  std::string source = temp.first;
+  int idx = temp.second;
+  std::string res = "";
+
+  // total
+  for (; source[idx] != ','; idx++) {
+    if (source[idx] >= '0' && source[idx] <= '9' || source[idx] == '.') {
+      res += source[idx];
+    }
+  }
+  json_info["system"]["physical memory(MiB)"]["total"] = atof(res.c_str());
+  idx++, res = "";
+
+  // free
+  for (; source[idx] != ','; idx++) {
+    if (source[idx] >= '0' && source[idx] <= '9' || source[idx] == '.') {
+      res += source[idx];
+    }
+  }
+  json_info["system"]["physical memory(MiB)"]["free"] = atof(res.c_str());
+  idx++, res = "";
+
+  // used
+  for (; source[idx] != ','; idx++) {
+    if (source[idx] >= '0' && source[idx] <= '9' || source[idx] == '.') {
+      res += source[idx];
+    }
+  }
+  json_info["system"]["physical memory(MiB)"]["used"] = atof(res.c_str());
+  idx++, res = "";
+
+  // buff/cache
+  for (; source[idx] != 'b'; idx++) {
+    if (source[idx] >= '0' && source[idx] <= '9' || source[idx] == '.') {
+      res += source[idx];
+    }
+  }
+  json_info["system"]["physical memory(MiB)"]["buff/cache"] = atof(res.c_str());
+  return;
+}
+
+void system_cpus(json &json_info, const std::pair<std::string, int> &temp) {
+  std::string source = temp.first;
+  int idx = temp.second;
+  std::string res = "";
+
+  // user
+  for (; source[idx] != ','; idx++) {
+    if (source[idx] >= '0' && source[idx] <= '9' || source[idx] == '.') {
+      res += source[idx];
+    }
+  }
+  idx++;
+  json_info["system"]["Cpu(s)"]["user"] = atof(res.c_str());
+  res = "";
+
+  // system
+  for (; source[idx] != ','; idx++) {
+    if (source[idx] >= '0' && source[idx] <= '9' || source[idx] == '.') {
+      res += source[idx];
+    }
+  }
+  idx++;
+  json_info["system"]["Cpu(s)"]["system"] = atof(res.c_str());
+  res = "";
+
+  // niced
+  for (; source[idx] != ','; idx++) {
+    if (source[idx] >= '0' && source[idx] <= '9' || source[idx] == '.') {
+      res += source[idx];
+    }
+  }
+  idx++;
+  json_info["system"]["Cpu(s)"]["nice"] = atof(res.c_str());
+  res = "";
+
+  // idle
+  for (; source[idx] != ','; idx++) {
+    if (source[idx] >= '0' && source[idx] <= '9' || source[idx] == '.') {
+      res += source[idx];
+    }
+  }
+  idx++;
+  json_info["system"]["Cpu(s)"]["idle"] = atof(res.c_str());
+  res = "";
+
+  // IO wait
+  for (; source[idx] != ','; idx++) {
+    if (source[idx] >= '0' && source[idx] <= '9' || source[idx] == '.') {
+      res += source[idx];
+    }
+  }
+  idx++;
+  json_info["system"]["Cpu(s)"]["IO wait"] = atof(res.c_str());
+  res = "";
+
+  // hardware IRQ usage
+  for (; source[idx] != ','; idx++) {
+    if (source[idx] >= '0' && source[idx] <= '9' || source[idx] == '.') {
+      res += source[idx];
+    }
+  }
+  idx++;
+  json_info["system"]["Cpu(s)"]["hardware IRQ"] = atof(res.c_str());
+  res = "";
+
+  // software IRQ
+  for (; source[idx] != ','; idx++) {
+    if (source[idx] >= '0' && source[idx] <= '9' || source[idx] == '.') {
+      res += source[idx];
+    }
+  }
+  idx++;
+  json_info["system"]["Cpu(s)"]["software IRQ"] = atof(res.c_str());
+  res = "";
+
+  // hpyervisor
+  for (; source[idx] != 's'; idx++) {
+    if (source[idx] >= '0' && source[idx] <= '9' || source[idx] == '.') {
+      res += source[idx];
+    }
+  }
+  idx++;
+  json_info["system"]["Cpu(s)"]["hpyervisor"] = atof(res.c_str());
+}
+
+void system_tasks(json &json_info, const std::pair<std::string, int> &temp) {
+  std::string source = temp.first;
+  int idx = temp.second + 1;
+  std::string res = "";
+
+  // total
+  for (; source[idx] != ','; idx++) {
+    if (source[idx] >= '0' && source[idx] <= '9') {
+      res += source[idx];
+    }
+  }
+  idx++;
+  json_info["system"]["Tasks"]["total"] = atoi(res.c_str());
+  res = "";
+
+  // running
+  for (; source[idx] != ','; idx++) {
+    if (source[idx] >= '0' && source[idx] <= '9') {
+      res += source[idx];
+    }
+  }
+  idx++;
+  json_info["system"]["Tasks"]["running"] = atoi(res.c_str());
+  res = "";
+
+  // sleeping
+  for (; source[idx] != ','; idx++) {
+    if (source[idx] >= '0' && source[idx] <= '9') {
+      res += source[idx];
+    }
+  }
+  idx++;
+  json_info["system"]["Tasks"]["sleeping"] = atoi(res.c_str());
+  res = "";
+
+  // stopped
+  for (; source[idx] != ','; idx++) {
+    if (source[idx] >= '0' && source[idx] <= '9') {
+      res += source[idx];
+    }
+  }
+  idx++;
+  json_info["system"]["Tasks"]["stopped"] = atoi(res.c_str());
+  res = "";
+
+  // zombie
+  for (; source[idx] != 'z'; idx++) {
+    if (source[idx] >= '0' && source[idx] <= '9') {
+      res += source[idx];
+    }
+  }
+  json_info["system"]["Tasks"]["zombie"] = atoi(res.c_str());
+}
+
+void setSystem_load_average(json &json_info,
+                            const std::pair<std::string, int> &temp) {
+  std::string source = temp.first;
+  int idx = temp.second + 1;
+  std::string res = "";
+  for (;; idx++) {
+    if (source[idx] >= '0' && source[idx] <= '9' || source[idx] == '.') {
+      res += source[idx];
+    }
+    if (source[idx] == ',') {
+      idx++;
+      break;
+    }
+  }
+  json_info["system"]["1 min load average"] = atof(res.c_str());
+  res = "";
+
+  for (;; idx++) {
+    if (source[idx] >= '0' && source[idx] <= '9' || source[idx] == '.') {
+      res += source[idx];
+    }
+    if (source[idx] == ',') {
+      idx += 2;
+      break;
+    }
+  }
+  json_info["system"]["5 min load average"] = atof(res.c_str());
+  res = "";
+
+  for (;; idx++) {
+    if (source[idx] >= '0' && source[idx] <= '9' || source[idx] == '.') {
+      res += source[idx];
+    } else {
+      json_info["system"]["15 min load average"] = atof(res.c_str());
+      return;
+    }
+  }
+  return;
 }
 
 std::pair<std::string, int> findFromFile_system_info(const std::string &source,
